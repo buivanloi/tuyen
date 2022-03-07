3a7f071c1e9030a277b37aa000946c60f62a6cd9
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-07 11:03:39 +0700

    Add files via upload
    8/3

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/thituyen.pham.7921';
+const eventName = 'Bùi Lợi chúc mừng ngày 8-3';
+const titleCard = 'Tặng cô gái';
+const contentCard = 'Nhân ngày 8/3 anh chúc em sẽ mãi luôn hạnh phúc. Bởi lúc sinh ra bố mẹ đã cho mình 2 thứ quý giá. 1 là đôi mắt để thấy được cuộc sống tươi đẹp này, 2 là trái tim để cảm nhận tình yêu thương. Thế nên phải luôn yêu chính bản thân mình, và  nếu có một ngày trái tim em bị chính con mắt của mình làm tổn thương thì đừng quên rằng thế giới này còn cho em điều quý giá thứ 3 nữa...đó chính là Bùi Lợi. Yên tâm. Ký tên Lợi sến súa';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'tuyen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
